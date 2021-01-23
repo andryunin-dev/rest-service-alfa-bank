83f5cf9dbaa84a4de41111a62b7ac1e7374b3670
Andrey Andryunin <andryunin.dev@gmail.com>
2021-01-23 16:23:00 +0400

    initial commit

---
+++ src/main/java/com/example/restalfabank/RestAlfaBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.restalfabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAlfaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAlfaBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restalfabank/RestAlfaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restalfabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAlfaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
